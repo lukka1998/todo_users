b80b24f8d822cec88a7cb65fad1cdea663f7b210
luka makaridze <122208361+lukka1998@users.noreply.github.com>
2024-10-18 19:11:23 +0400

    commit finish

---
+++ index.js
@@ -0,0 +1,62 @@
+async function getAlltodos(){
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    console.log(response.data);
+    return response.data
+
+}
+async function getUsers(){
+    const response = await axios.get("https://jsonplaceholder.typicode.com/users");
+    console.log(response.data);
+    return response.data
+}
+async function main() {
+    let todos = await getAlltodos();
+    let users = await getUsers();
+    renderhtml(todos ,users);
+}
+async function renderhtml(todos ,users ){
+    let html = `
+    <div class="item">
+            <div class="left">
+                <input type="checkbox">
+                <p>Image</p>
+                <p class = "title">Title</p>
+            </div>
+            <p>Email</p>
+            <p>Adress</p>
+            <p>zipcode</p>
+
+            <p>Status</p>
+            <p>Delete</p>
+        </div>`
+    for(let i = 0 ;i < 10; i++){
+        html+=
+        `<div class="item margin">
+        <div class="left">
+            <input type="checkbox">
+            <img src="images/f1b9162386ccc9e2100b78f333fed351.png" alt="">
+            <p>${todos[i].title}</p>
+        </div>
+        <p>${users[i].email}</p>
+        <p>${users[i].address.street}</p>
+        <p>${users[i].address.zipcode}</p>
+        <p>${ todos[i].completed ? `<p class = "true">success<p>` : `<p class = "false">false<p>` }</p>
+         <img class="button" src="images/trash-bin.png" alt="">
+        
+    </div>
+
+        `
+
+    }
+document.querySelector(".main").innerHTML = html
+}
+main();
+document.querySelector(".button").addEventListener("click" ,function(){
+    document.querySelector(".item").style.display = "none"
+} );
+async function deletetodos(todoId){
+    //axios do not work on the api when use delete method
+    axios.delete(`https://app-servers.io/api/todos/delete/${todoId}`,)
+     let todos = await getAlltodos();
+     renderhtml(todos);
+}
